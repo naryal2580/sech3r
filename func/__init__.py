7dab4ba3bf8b02df5c14ea218371af6af7bbf74f
naryal2580 <naryal2580@gmail.com>
2020-02-24 23:56:34 +0545

    washed everything

--- func/__init__.py
+++
@@ -1,4 +0,0 @@
-from .style import banner, printTakenInput
-from naryal2580.style import fetchFormattedTime, info, coolInput, coolExit, bad
-from .sech3rFunc import getHTTPheaders, checkVersionDisclosure, \
-  checkSecurityHeaders
