d92563ec059d4f4fd656c023bb1344e0f8b1ece4
naryal2580 <naryal2580@gmail.com>
2020-06-11 17:23:47 +0545

    mics change, after soyled updated

--- secher/__init__.py
+++ secher/__init__.py
@@ -4,4 +4,4 @@ from .extra import *
 __author__ = "naryal2580"
 __version__ = "4.4"
 
-del bad, blink, blue, blue_l, bold, core, cyan, cyan_l, dim, escSeqs, extra, genStyls, good, green, green_l, hidden, invert, italic, nGenStyls, name, NoRedirects, normal, purple, purple_l, red, red_l, request, rst, strike, style, uline, webrowser_openNewTab, white, white_l, yellow, yellow_l
+del bad, blink, blue, blue_l, bold, core, cyan, cyan_l, dim, extra, good, green, green_l, hidden, invert, italic, name, NoRedirects, normal, purple, purple_l, red, red_l, request, rst, strike, style, uline, webrowser_openNewTab, white, white_l, yellow, yellow_l
